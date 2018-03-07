96c026197eb53a4aaa6fd64a83484d4fbe23afbe
Joseph Presley <jpresley@omniex.io>
2018-03-07 06:56:57 -0800

    Update omniex environment variables

--- .omniex.sh
+++ .omniex.sh
@@ -5,4 +5,11 @@ alias build_ems='docker-compose exec omniex ./build.sh -j 2'
 alias build_clean='sudo rm -rf debug_build'
 alias build_clean_cmake='find . -name "*.cmake" -exec rm {} \;'
 alias ems='~/workspace/omniex/ems'
+alias e='ems'
 alias rebuild_ems='build_clean;build_ems'
+
+export EMS_SRC="$HOME/workspace/omniex/ems"
+export LBM_LICENSE_FILENAME="${EMS_SRC}/29west.lic"
+export LBM_DEFAULT_CONFIG_FILE="$EMS_SRC}/etc/lbtrm.cfg"
+export LD_LIBRARY_PATH=/opt/googletest-1.8.0/lib:/opt/rh/devtoolset-7/root:${LD_LIBRARY_PATH}
+
