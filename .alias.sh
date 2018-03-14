dd033d3340c28635840a5e45e1516e10dad09e33
Joseph Presley <jpresley@omniex.io>
2018-03-14 07:54:37 -0700

    add more git aliases

--- .alias.sh
+++ .alias.sh
@@ -22,10 +22,12 @@ alias ga='git add -A'
 alias gb='git branch -a'
 alias gc='git commit -m'
 alias gca='git add -A && git commit -m'
+alias gco='git checkout'
 alias gcount='git rev-list master.. --count' # numbers of commits since master
 alias gdiff='git diff'
 alias gd='gdiff'
 alias gm='git checkout master'
+alias gnb='git checkout -b'
 alias gp='git remote prune origin'
 alias gpl='git branch --merged master | grep -v 'master$' | xargs -r git branch -d'
 alias gpo='git push origin $(git symbolic-ref -q --short HEAD)'
@@ -33,10 +35,12 @@ alias gpof='git push -f origin $(git symbolic-ref -q --short HEAD)'
 alias gpou='git push -u origin $(git symbolic-ref -q --short HEAD)'
 alias gr='cd $(git rev-parse --show-toplevel)' # takes you to root of git repo
 alias gri='git rebase -i HEAD~"$(gcount)"' # interactive rebase entire branch
+alias grm='git rebase master'
 alias gs='git status'
 alias gsc='git stash clear'
 alias gsl='git stash list'
 alias gst='git stash'
+alias gu='git pull' # git update
 
 alias ll='exa -al --color=always --git'
 
